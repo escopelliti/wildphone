43efe108821abbd99f86957f28fe4d74741372df
leonardthebest <leonardthebest@06dc2ac8-a1e0-1dd6-3df3-4087e5409a6c>
2012-05-07 14:22:48 +0000

    Roster

--- WildPhone/src/HomeUser/RosterManager.java
+++ WildPhone/src/HomeUser/RosterManager.java
@@ -4,9 +4,13 @@
  */
 package HomeUser;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import org.jivesoftware.smack.Connection;
 import org.jivesoftware.smack.Roster;
-import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.RosterEntry;
+import org.jivesoftware.smack.RosterListener;
+import org.jivesoftware.smack.packet.Presence;
 
 /**
  * present e il roster per gli utenti
@@ -14,16 +18,52 @@ import org.jivesoftware.smack.XMPPConnection;
  */
 public class RosterManager {
         private  Roster rs;
-        private  Connection conn;
-        //private final RosterEventsListener rosterEventListener;
+        private  Connection xmppconn;
+        private Presence presence;
+        
+        private ArrayList users;
+        //private  RosterEventsListener rosterEventListener;
+        Collection<RosterEntry> entries;
     public RosterManager (Connection xmppconn){
+        rs.setSubscriptionMode(Roster.SubscriptionMode.accept_all);
         try{
-            this.conn=xmppconn;
-            this.rs=xmppconn.getRoster();
-            
+            this.xmppconn=xmppconn;
+            rs=xmppconn.getRoster();
+            entries = rs.getEntries();
+            users=(ArrayList)entries; //Casting di entries in un vettore
+            //visualizzo nella shell gli utenti collegati
+            System.out.println("Utenti Presenti: "+rs.getEntryCount());
+            for(RosterEntry entry : entries){
+                System.out.println("NAME: "+entry.getName()+"\nUSER: "+entry.getUser()+"\n\n");
+            }
         }
         catch (AbstractMethodError e) {
             System.out.println("EXCEPTION:"+e.getMessage());
         }
+        
+        //Roster Listener
+            rs.addRosterListener(new RosterListener() {
+
+            @Override
+            public void entriesAdded(Collection<String> addresses) {
+                throw new UnsupportedOperationException("Not supported yet.");
+            }
+
+            @Override
+            public void entriesUpdated(Collection<String> addresses) {
+                throw new UnsupportedOperationException("Not supported yet.");
+            }
+
+            @Override
+            public void entriesDeleted(Collection<String> addresses) {
+                throw new UnsupportedOperationException("Not supported yet.");
+            }
+
+            @Override
+            public void presenceChanged(Presence presence) {
+                //throw new UnsupportedOperationException("Not supported yet.");
+                System.out.println("Presence changed: " + presence.getFrom() + " " + presence);
+            }
+        });
     }
 }

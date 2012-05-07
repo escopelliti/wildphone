c9fd8bd946cd92c446e108f530c906d371a0968f
leonardthebest <leonardthebest@06dc2ac8-a1e0-1dd6-3df3-4087e5409a6c>
2012-05-07 09:23:40 +0000

    Roster

---
+++ WildPhone/src/HomeUser/RosterManager.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package HomeUser;
+
+import org.jivesoftware.smack.Roster;
+import org.jivesoftware.smack.XMPPConnection;
+
+/**
+ *
+ * @author leox
+ */
+public class RosterManager {
+        private  Roster rs;
+        private  XMPPConnection conn;
+        //private final RosterEventsListener rosterEventListener;
+    public RosterManager (XMPPConnection xmppconn){
+        try{
+            this.conn=xmppconn;
+            this.rs=xmppconn.getRoster();
+        }
+        catch (AbstractMethodError e) {
+            System.out.println("EXCEPTION:"+e.getMessage());
+        }
+    }
+}
